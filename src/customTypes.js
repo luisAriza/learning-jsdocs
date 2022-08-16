fc8eff43a028d2bfd44eaf217d04cd928e9d37a0
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-16 15:17:32 -0500

    Custom types

---
+++ src/customTypes.js
@@ -0,0 +1,28 @@
+/**
+ * A new user object
+ * @typedef {object} user
+ * @property {number} id User id
+ * @property {string} name User's name
+ * @property {number | string} [age] User's age (optional)
+ * @property {boolean} isActive User state
+ */
+
+/**
+ * @type {user}
+ */
+const myNewUser = {
+  id: 1,
+  name: "Luis",
+  age: 30,
+  isActive: true,
+};
+
+/**
+ * @type {user}
+ */
+const myNewUser2 = {
+  id: 2,
+  name: "Jose",
+  age: 20,
+  isActive: false,
+};
