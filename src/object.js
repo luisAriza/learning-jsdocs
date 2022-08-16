6514b5094c8819b6b66c8ca558b8800f3d702b5a
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-15 20:15:37 -0500

    Object documentation

---
+++ src/object.js
@@ -0,0 +1,22 @@
+/**
+ * Objeto literal persona
+ * @type {{id: number, firstName: string, lastName: string, age: number}}
+ */
+const person = {
+  id: 1,
+  firstName: "Luis",
+  lastName: "Ariza",
+  age: 30,
+};
+
+/**
+ * Objeto literal persona 2
+ * |
+ * @type {{id: number | string, firstName: string, lastName: string, age: number}}
+ */
+const person2 = {
+  id: 2,
+  firstName: "Yordana",
+  lastName: "Herrera",
+  age: 18,
+};
