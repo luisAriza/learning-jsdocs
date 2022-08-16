b38e3d1918968ae52b586b829262c9a1fce31539
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-15 20:31:45 -0500

    Function documentation

---
+++ src/function.js
@@ -0,0 +1,16 @@
+/**
+ * Sumar dos números
+ * @param {number} n1 primer numero
+ * @param {number} n2 segundo numero
+ * @returns {number} La suma total
+ */
+function add(n1, n2) {
+  return n1 + n2;
+}
+/**
+ * Add two numbers
+ * @param {number} n1 First number
+ * @param {number} n2 Second number
+ * @returns {string} Total added
+ */
+const addTwo = (n1, n2) => `The result is: ${n1 + n2}`;
