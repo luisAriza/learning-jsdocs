ed7a3ae28a0b200f4609999cdc61a8374448a025
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-17 18:29:12 -0500

    Home documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# My project
+This is my awesome project
+
+* Link 1
+* Link 2
+* Link 3
+* Link 4
---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * @file index.js the main file
+ * @author Luis Ariza
+ * @see <a href="https://github.com/luisAriza">GitHub</a>
+ */
